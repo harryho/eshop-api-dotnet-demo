5b14e7a64bfb8987b6283de051b729dc96045218
harryho <harry.ho_long@yahoo.com>
2025-10-12 20:31:37 +1100

    Move back to Net 8 LTS

--- Eshop.Api/Endpoints/ProductsEndpoints.cs
+++ Eshop.Api/Endpoints/ProductsEndpoints.cs
@@ -40,56 +40,6 @@ public static class ProductsEndpoints
        .RequireAuthorization(Policies.AdminReadAccess)
        .MapToApiVersion(2.0);
 
-        // group.MapPost("/", async Task<CreatedAtRoute<ProductDtoV1>> (IProductsRepository repository, CreateProductDto productDto) =>
-        // {
-        //     Product product = new()
-        //     {
-        //         Name = productDto.Name,
-        //         Genre = productDto.Genre,
-        //         UnitPrice = productDto.UnitPrice,
-        //         UnitInStock = productDto.UnitInStock,
-        //         ReleaseDate = productDto.ReleaseDate,
-        //         ImageUri = productDto.ImageUri
-        //     };
-
-        //     await repository.CreateAsync(product);
-        //     return TypedResults.CreatedAtRoute(product.AsDtoV1(), GetProductV1EndpointName, new { id = product.Id });
-
-        // })
-        // .RequireAuthorization(Policies.StaffWriteAccess)
-        // .MapToApiVersion(1.0);
-
-
-        // group.MapPut("/{id}", async Task<Results<NoContent, NotFound>> (IProductsRepository repository, int id, UpdateProductDto product) =>
-        // {
-        //     var existingProduct = await repository.GetAsync(id);
-        //     if (existingProduct is null)
-        //     {
-        //         return TypedResults.NotFound();
-        //     }
-        //     existingProduct.Name = product.Name;
-        //     existingProduct.UnitPrice = product.UnitPrice;
-        //     existingProduct.UnitInStock = product.UnitInStock;
-
-        //     await repository.UpdateAsync(existingProduct);
-        //     return TypedResults.NoContent();
-        // })
-        // .RequireAuthorization(Policies.StaffWriteAccess)
-        // .MapToApiVersion(1.0);
-
-        // group.MapDelete("/{id}", async (IProductsRepository repository, int id) =>
-        // {
-        //     var existingProduct = await repository.GetAsync(id);
-        //     if (existingProduct is not null)
-        //     {
-        //         await repository.DeleteAsync(id);
-        //     }
-
-        //     return TypedResults.NoContent();
-        // })
-        // .RequireAuthorization(Policies.AdminWriteAccess)
-        // .MapToApiVersion(1.0);
-
         group.MapPost("/", CreateProduct)
         .RequireAuthorization(Policies.AdminWriteAccess)
         .MapToApiVersion(1.0);
